6ddcecb20ae41c141f90f2411e258ddf40e1425b
Sean Gravener <sean@gravener.net>
2024-01-21 18:15:03 -0500

    Tidy up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameBoardComponent } from './components/board.component';
+import { GameBoardComponent } from './board/board.component';
+import { MaterialModule } from './mat-ui/mat-ui.module';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameBoardComponent
-  ],
+  declarations: [AppComponent, GameBoardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // MaterialModule,
+    MatChipsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
